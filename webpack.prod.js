fd84a259463ef70e7584f89b963f04145ce64cd2
Sergei Zheleznov <sksdes@gmail.com>
2020-03-28 13:11:24 +0100

    removed uglifyjs-webpack-plugin; added terser-webpack-plugin; gh-pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const merge = require("webpack-merge");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin")
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
@@ -22,7 +22,7 @@ module.exports = merge(common, {
 
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
+      new TerserPlugin({
         cache: true,
         parallel: true,
         sourceMap: true
