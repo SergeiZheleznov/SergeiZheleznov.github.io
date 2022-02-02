20675ba7d54b7c12499f1e44aec087f2e079a493
Sergei Zheleznov <sergei.zheleznov@outlook.com>
2022-02-02 19:50:15 +0100

    feat: add custom fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,21 @@ import Title from '@/components/Title';
 
 export default function Home() {
   return (
-    <main className="min-h-screen dark:(bg-slate-800 text-white)">
+    <main className="min-h-screen dark:(bg-zinc-900 text-white)">
       <Helmet>
         <html className="dark" lang="en" />
-        <body className="text-lg" />
+        <body className="h-screen w-full text-base" />
       </Helmet>
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <Title>Hello TypeScript!</Title>
-        <p className="font-sans text-lg">Sample text (Sans)</p>
-        <p>Another sample</p>
+      <div className="h-screen flex items-center justify-center">
+        <div className="max-w-3xl">
+          <Title>
+            Hello <span className="font-extrabold">TypeScript!</span>
+          </Title>
+          <p className="font-sans text-lg">Sample text</p>
+          <p>
+            Another <span className="font-bold">sample</span>
+          </p>
+        </div>
       </div>
     </main>
   );
--- twind.config.js
+++ twind.config.js
@@ -4,15 +4,34 @@ import { defineConfig } from '@twind/tailwind';
 export default defineConfig({
   darkMode: 'class',
   theme: {
+    fontSize: {
+      base: '1.8rem',
+      lg: '3rem',
+    },
     extend: {
       fontFamily: {
-        sans: ["'Source Code Pro'"],
-        body: "'Inconsolata'",
-        display: "'Inconsolata'",
+        sans: '"Jet Brains Mono", sans-serif',
       },
     },
   },
   preflight: {
-    '@import': `url('https://fonts.googleapis.com/css2?family=Inconsolata&family=Source+Code+Pro:wght@400;600&display=swap')`,
+    // '@import': `url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400&display=swap')`,
+    '@font-face': [
+      {
+        fontFamily: 'Jet Brains Mono',
+        fontWeight: '400',
+        src: 'url(/fonts/JetBrainsMono-Regular.woff2) format("woff")',
+      },
+      {
+        fontFamily: 'Jet Brains Mono',
+        fontWeight: '700',
+        src: 'url(/fonts/JetBrainsMono-Bold.woff2) format("woff")',
+      },
+      {
+        fontFamily: 'Jet Brains Mono',
+        fontWeight: '800',
+        src: 'url(/fonts/JetBrainsMono-ExtraBold.woff2) format("woff")',
+      },
+    ],
   },
 });
