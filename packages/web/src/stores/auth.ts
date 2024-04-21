5657065a0f616e042555ac6f1afc5185611dbc8f
Sergei Zheleznov <sergei.zheleznov@outlook.com>
2024-04-21 11:38:32 +0200

    clean up

--- packages/web/src/stores/auth.ts
+++ packages/web/src/stores/auth.ts
@@ -1,4 +1,4 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { supabase } from '@/utils/supabase'
 import type { User } from '@supabase/supabase-js'
@@ -7,7 +7,7 @@ export const useAuthStore = defineStore('auth', () => {
   const user = ref<User | null>()
 
   const authenticate = async (email: string, password: string) => {
-    const { data, error } = await supabase.auth.signInWithPassword({ email, password })
+    const { data } = await supabase.auth.signInWithPassword({ email, password })
 
     user.value = data.user
   }
